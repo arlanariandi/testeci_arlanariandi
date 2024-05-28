69a801be7493413455fd52fa69ef18da4e3e107f
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2024-05-28 20:30:03 +0700

    UPDATE: Add falidation input

--- testeci-fe/src/pages/ConvertToWords/index.jsx
+++ testeci-fe/src/pages/ConvertToWords/index.jsx
@@ -1,21 +1,28 @@
 import {useState} from "react";
 import axios from "axios";
+import * as Yup from "yup";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 
 const convertToWords = () => {
-    const [number, setNumber] = useState('');
     const [terbilang, setTerbilang] = useState('');
+    const [inputNumber, setInputNumber] = useState(0);
 
-    const handleChange = (e) => {
-        setNumber(e.target.value);
-    }
+    const initialValues = {
+        number: 0,
+    };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const validationSchema = Yup.object({
+        number: Yup.number().required('Number is required').positive('Number must be a positive number').integer('Number must be an integer'),
+    });
+
+    const handleSubmit = async (values, {setSubmitting}) => {
         try {
-            const response = await axios.post("/api/convert", {number});
+            const response = await axios.post("/api/convert", values);
             setTerbilang(response.data.terbilang);
         } catch (e) {
-            console.log(e)
+            console.error('Error creating number', e);
+        } finally {
+            setSubmitting(false);
         }
     }
 
@@ -24,28 +31,41 @@ const convertToWords = () => {
             <h1 className="text-2xl font-bold text-gray-800">Number to Words Converter</h1>
             <hr className="my-8"/>
             <div className="grid gap-16 md:grid-cols-2 ">
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="input"
-                               className="block mb-2 text-sm font-medium text-gray-900">Input Number</label>
-                        <input type="text" id="input" name="input"
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                               placeholder="33839494xxxxx" required
-                               value={number}
-                               onChange={handleChange}/>
-                    </div>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}>
+                    {({isSubmitting, setFieldValue}) => (
+                        <Form>
+                            <div>
+                                <label htmlFor="number"
+                                       className="block mb-2 text-sm font-medium text-gray-900">Input Number</label>
+                                <Field type="text" id="number" name="number"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                       onChange={(e) => {
+                                           const value = e.target.value;
+                                           setFieldValue('number', value);
+                                           setInputNumber(value); // update state inputNumber
+                                       }}
+                                />
+                                <ErrorMessage name="number" component="div" className="mt-4 text-sm text-red-800"/>
+                            </div>
 
-                    <button type="submit"
-                            className="mt-4 text-white font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-teal-500 hover:bg-teal-700">
-                        Convert
-                    </button>
-                </form>
+                            <button type="submit"
+                                    className="mt-4 text-white font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-teal-500 hover:bg-teal-700"
+                                    disabled={isSubmitting}
+                            >
+                                Convert
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
 
                 <div>
                     <h2 className="text-lg font-semibold text-gray-800 mb-4">Output</h2>
                     {terbilang && (
                         <div>
-                            <p className="text-base text-gray-700 mb-2">Rp. {number}</p>
+                            <p className="text-base text-gray-700 mb-2">Rp. {inputNumber}</p>
                             <p className="text-base text-gray-700 mb-4">Terbilang:</p>
                             <div className="font-semibold text-gray-800 capitalize">{terbilang}</div>
                         </div>
--- testeci-fe/src/pages/Triangle/index.jsx
+++ testeci-fe/src/pages/Triangle/index.jsx
@@ -1,26 +1,28 @@
 import {useState} from "react";
 import axios from "axios";
+import * as Yup from "yup";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 
 const Triangle = () => {
-    const [rows, setRows] = useState(0);
-    const [type, setType] = useState('type1');
     const [triangle, setTriangle] = useState('');
 
-    const handleChangeRows = (e) => {
-        setRows(e.target.value);
-    }
+    const initialValues = {
+        rows: 0,
+        type: 'type1'
+    };
 
-    const handleChangeType = (e) => {
-        setType(e.target.value);
-    }
+    const validationSchema = Yup.object({
+        rows: Yup.number().required('Rows is required').positive('Rows must be a positive number').integer('Rows must be an integer'),
+    });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const handleSubmit = async (values, {setSubmitting}) => {
         try {
-            const response = await axios.post("/api/generate-triangle", {rows, type});
+            const response = await axios.post("/api/generate-triangle", values);
             setTriangle(response.data);
         } catch (e) {
             console.error('Error creating triangle', e);
+        } finally {
+            setSubmitting(false);
         }
     }
 
@@ -29,40 +31,52 @@ const Triangle = () => {
             <h1 className="text-2xl font-bold text-gray-800">Triangle</h1>
             <hr className="my-8"/>
             <div className="grid gap-16 md:grid-cols-2">
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <div className="mb-6">
-                            <label htmlFor="rows"
-                                   className="block mb-2 text-sm font-medium text-gray-900">Type of triangle</label>
-                            <select value={type} onChange={handleChangeType}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                                <option value="type1">Type 1</option>
-                                <option value="type2">Type 2</option>
-                                <option value="type3">Type 3</option>
-                            </select>
-                        </div>
-                    </div>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={handleSubmit}>
+                    {({isSubmitting}) => (
+                        <Form>
+                            <div className="mb-4">
+                                <div className="mb-6">
+                                    <label htmlFor="rows"
+                                           className="block mb-2 text-sm font-medium text-gray-900">Type of
+                                        triangle</label>
+                                    <Field as="select" name="type"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                        <option value="type1">Type 1</option>
+                                        <option value="type2">Type 2</option>
+                                        <option value="type3">Type 3</option>
+                                    </Field>
+                                </div>
+                            </div>
 
-                    <div className="mb-4">
-                        <div className="mb-6">
-                            <label htmlFor="rows"
-                                   className="block mb-2 text-sm font-medium text-gray-900">Rows</label>
-                            <input type="text" id="rows" name="rows"
-                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                   value={rows}
-                                   onChange={handleChangeRows}
-                            />
-                        </div>
-                    </div>
+                            <div className="mb-4">
+                                <div className="mb-6">
+                                    <label htmlFor="rows"
+                                           className="block mb-2 text-sm font-medium text-gray-900">Rows</label>
+                                    <Field type="text" name="rows"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                                    <ErrorMessage name="rows" component="div" className="mt-4 text-sm text-red-800"/>
+                                </div>
+                            </div>
 
-                    <div className="flex items-center justify-between">
-                        <button
-                            className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
-                            type="submit">
-                            Generate Triangle
-                        </button>
-                    </div>
-                </form>
+                            <div className="flex items-center justify-between">
+                                <button
+                                    className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:shadow-outline"
+                                    type="submit"
+                                    disabled={isSubmitting}
+                                >
+                                    Generate Triangle
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+
+                {/*<form onSubmit={handleSubmit}>*/}
+
+                {/*</form>*/}
 
                 <div>
                     <h2 className="text-xl font-semibold text-gray-800 mb-4">Output</h2>
